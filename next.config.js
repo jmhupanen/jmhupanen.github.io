2f7d3df44c73ae025585e01e33351c87b1c740cd
jmhupanen <juho.hupanen@hotmail.fi>
2022-12-11 22:54:39 +0200

    Use index.tsx instead of app dir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  }
+  // experimental: {
+  //   appDir: true,
+  // }
 }
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>Hi 👋, <br /> my name is Juho and this is my page.</div>
+  );
+}
+
+export default Home;
