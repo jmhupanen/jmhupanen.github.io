188bed83486bde8346bcbb6e6f6df185ac12388d
jmhupanen <juho.hupanen@hotmail.fi>
2022-12-11 21:56:21 +0200

    Initialize Next.js & GitHub workflow

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <p>I am a header and I serve absolutely no purpose</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>Hi 👋, <br /> my name is Juho and this is my page.</div>
+  );
+}
+
+export default Home;
